c58542a666e8511efe2df5fca8a63db1ca27eec8
Douglas <douglas-negocios@hotmail.com>
2023-04-19 18:39:03 -0300

    Importação do AutocompleteModule e FormsModule para Shared Module

--- src/app/_shared/shared/shared.module.ts
+++ src/app/_shared/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AutoCompleteComponent} from "./auto-complete/auto-complete.component";
+import {AutoCompleteModule} from "primeng/autocomplete";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [AutoCompleteComponent],
@@ -9,6 +11,8 @@ import {AutoCompleteComponent} from "./auto-complete/auto-complete.component";
     ],
     imports: [
         CommonModule,
+        AutoCompleteModule,
+        FormsModule,
     ]
 })
 export class SharedModule { }
