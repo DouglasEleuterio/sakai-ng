4ab0d3e3d062fbc17810534ceee500d3bf6ff1c7
Douglas <douglas-negocios@hotmail.com>
2023-05-01 18:15:18 -0300

    Obtendo lista de formas de pagamento

--- src/app/service/forma-pagamento.service.ts
+++ src/app/service/forma-pagamento.service.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AutoCompleteService} from "./auto-complete.service";
 import {HttpClient} from "@angular/common/http";
 import {FormaPagamentoModel} from "../_model/forma-pagamento.model";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment.prod";
 
 const FORMA_PAGAMENTO_RESOURCE: string = 'forma-pagamento'
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class FormaPagamentoService extends AutoCompleteService<FormaPagamentoModel> {
+export class FormaPagamentoService extends AutoCompleteService<FormaPagamentoModel>{
 
     constructor(http: HttpClient) {
         super(http, FORMA_PAGAMENTO_RESOURCE);
+    }
+
+    public findList(): Observable<any> {
+        return this.http.get(environment.apiUrl + `${FORMA_PAGAMENTO_RESOURCE}/find-list`)
     }
 }
