8e34787506c96fc9722c4e2fe3be5dad0f21ec11
Douglas <douglas-negocios@hotmail.com>
2023-05-01 18:14:39 -0300

    Atualizando forma de pegar pagamento selecionado

--- src/app/service/pagamentos.service.ts
+++ src/app/service/pagamentos.service.ts
@@ -1,23 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {environment} from "../../environments/environment.prod";
 import {Observable} from "rxjs";
 import {PagamentoModel} from "../_model/pagamento.model";
+import {FormaPagamentoModel} from "../_model/forma-pagamento.model";
 
 const PATH_URL_PAGAMENTOS = 'pagamento/'
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PagamentosService {
 
-  constructor(private http: HttpClient) { }
+    private pagamentoParaEditar: PagamentoModel = {}
 
-  public getAll(param: HttpParams): Observable<any> {
-      return this.http.get(environment.apiUrl + PATH_URL_PAGAMENTOS + 'v2/find-list', {params: param})
-  }
+    constructor(private http: HttpClient) {}
+
+    public getAll(param: HttpParams): Observable<any> {
+        return this.http.get(environment.apiUrl + PATH_URL_PAGAMENTOS + 'v2/find-list', {params: param})
+    }
 
     atualizarPagamento(pagamento: PagamentoModel): Observable<any> {
         return this.http.put(environment.apiUrl + PATH_URL_PAGAMENTOS + 'atualizar/' + pagamento.id, pagamento)
+    }
+
+    getFormaPagamentoParaEditar(): FormaPagamentoModel {
+        return <FormaPagamentoModel>this.pagamentoParaEditar;
+    }
+
+    setPagamentoParaEditar(pagamento: PagamentoModel){
+        this.pagamentoParaEditar = pagamento
     }
 }
