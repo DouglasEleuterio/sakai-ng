87086f724f6768adec7313a6c83fa0aa96dfa43a
Douglas <douglas-negocios@hotmail.com>
2023-05-03 12:13:55 -0300

    Configuração e Criação do Módulo de lançamento

--- src/app/demo/components/ctr/ctr-routing.module.ts
+++ src/app/demo/components/ctr/ctr-routing.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import {RouterModule} from "@angular/router";
 
 const routes = [
-    {
-        path: '', data: {breadcrumb: 'CTR'},
-        loadChildren: () => import('./pagina/pagina.module')
-            .then(m => m.PaginaModule)
-    }
+    { path: '', data: {breadcrumb: 'CTR'}, loadChildren: () => import('./pagina/pagina.module').then(m => m.PaginaModule)},
+    { path: 'lancar', data: {breadcrumb: 'Lançar'}, loadChildren: () => import('./lancar/lancar.module').then(m => m.LancarModule)}
 ]
 
 @NgModule({
