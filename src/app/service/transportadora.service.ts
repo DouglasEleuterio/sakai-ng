879d445b07c68f9b259c425b618a4678de0a9deb
Douglas <douglas-negocios@hotmail.com>
2023-05-04 00:49:16 -0300

    Chamando a nova versão da API 2.0

--- src/app/service/transportadora.service.ts
+++ src/app/service/transportadora.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {AutoCompleteService} from "./auto-complete.service";
 import {FormaPagamentoModel} from "../_model/forma-pagamento.model";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment.prod";
+import {TransportadorModel} from "../_model/transportador.model";
 
 const TRANSPORTADORA_RESOURCE: string = 'transportador'
 
@@ -12,5 +15,9 @@ export class TransportadoraService extends AutoCompleteService<FormaPagamentoMod
 
     constructor(http: HttpClient) {
         super(http, TRANSPORTADORA_RESOURCE);
+    }
+
+    override loadAutoCompleteData(): Observable<TransportadorModel[]> {
+        return this.http.get(environment.apiUrl +'v2/'+ TRANSPORTADORA_RESOURCE + '/find-list-from-select') as Observable<TransportadorModel[]>
     }
 }
--- src/app/service/veiculo.service.ts
+++ src/app/service/veiculo.service.ts
@@ -2,6 +2,8 @@
 import {AutoCompleteService} from "./auto-complete.service";
 import {VeiculoModel} from "../_model/veiculo.model";
 import {HttpClient} from "@angular/common/http";
+ import {Observable} from "rxjs";
+ import {environment} from "../../environments/environment.prod";
 
 const VEICULO_RESOURCE = 'veiculo'
 
@@ -13,4 +15,9 @@ export class VeiculoService extends AutoCompleteService<VeiculoModel>{
   constructor(http: HttpClient) {
       super(http, VEICULO_RESOURCE);
   }
+
+
+    override loadAutoCompleteData(): Observable<VeiculoModel[]> {
+        return this.http.get(environment.apiUrl +'v2/'+ VEICULO_RESOURCE + '/find-list-from-select') as Observable<VeiculoModel[]>
+    }
 }
