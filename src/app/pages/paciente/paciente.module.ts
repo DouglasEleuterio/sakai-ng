7c4fb238a3493835ca559003049fa15571d9f635
Douglas Eleutério <douglas-negocios@hotmail.com>
2024-04-10 19:01:08 -0300

    Atualização do Angular e PrimeNG.
    Estruturação da visualização dos dados do cliente.

--- src/app/pages/paciente/paciente.module.ts
+++ src/app/pages/paciente/paciente.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DetailComponent} from "./pages/detail/detail.component";
 import {PacienteRoutingModule} from "./paciente-routing.module";
+import {ButtonModule} from "primeng/button";
+import {StepperModule} from "primeng/stepper";
+
 
 @NgModule({
   declarations: [DetailComponent],
     imports: [
         CommonModule,
-        PacienteRoutingModule
+        PacienteRoutingModule,
+        ButtonModule,
+        StepperModule
     ]
 })
 export class PacienteModule { }
--- src/app/pages/paciente/pages/detail/detail.component.ts
+++ src/app/pages/paciente/pages/detail/detail.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss'
+  // styleUrl: './detail.component.scss',
+    styles: [
+        `
+        .p-stepper {
+            flex-basis: 50rem;
+        }
+        `
+    ]
 })
 export class DetailComponent {
 
